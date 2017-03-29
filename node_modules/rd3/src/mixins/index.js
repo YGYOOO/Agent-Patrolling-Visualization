5355227636423390fe459fd014b066aa6a7c262c
YGYOOO <594487125@qq.com>
2017-03-28 22:10:15 -0500

    init

--- node_modules/rd3/src/mixins/index.js
+++
@@ -1,5 +0,0 @@
-
-exports.CartesianChartPropsMixin = require('./CartesianChartPropsMixin');
-exports.DefaultAccessorsMixin = require('./DefaultAccessorsMixin');
-exports.ViewBoxMixin = require('./ViewBoxMixin');
-exports.TooltipMixin = require('./TooltipMixin');
